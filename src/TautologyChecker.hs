9f24f29a4fee64903bfda77adfe7eef07c0d6325
Reynaldo Cordero (naldoco) <reynaldo.cordero@gmail.com>
2017-01-03 17:01:55 +0100

    Red. Add eval

--- src/TautologyChecker.hs
+++ src/TautologyChecker.hs
@@ -4,8 +4,18 @@ data Prop = Const Bool
           | Var   Char
           | Not   Prop
           | And   Prop Prop
+          | Or    Prop Prop
           | Imply Prop Prop deriving Show
 
--- First past the post
+type Assoc k v = [(k,v)]
+type Subst = Assoc Char Bool
+
+find :: Eq k => k -> Assoc k v -> v
+find k t = head [v | (k', v) <- t, k == k']
+    
 isTaut :: Prop -> Bool
 isTaut _ = False
+
+eval :: Subst -> Prop -> Bool
+eval = undefined
+                     
--- test/Tests.hs
+++ test/Tests.hs
@@ -8,12 +8,24 @@ import TautologyChecker
 p1 :: Prop
 p1 = And (Var 'A') (Not  (Var 'A'))
 
+p7 :: Prop
+p7 = And (Var 'A') (Not  (Var 'B'))
+
+aFbT :: Subst
+aFbT = [('A', False), ('B', True)]
+
+
+
 tautologyCheckerSuite :: TestTree
 tautologyCheckerSuite =
   testGroup "Tautology checker tests"
     [ testGroup "isTaut"
         [ testCase ("isTaut "++(show p1)++" -> "++show False) $
             (isTaut p1) @?= False
+        ]
+    , testGroup "eval"
+        [ testCase ("eval ("++(show aFbT)++") ("++(show p7)++") -> "++show False) $
+            (eval aFbT p7) @?= False
         ]
     ]
 main = defaultMain tautologyCheckerSuite
